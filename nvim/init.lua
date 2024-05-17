59a51c68a2ebfd49f7097c2c62ab766f60e8b696
Benjamin Leung <benjamin.leung@outlook.com>
2024-05-18 00:34:03 +1000

    Tue 14 May 2024 22:13:46 AEST

--- nvim/init.lua
+++ nvim/init.lua
@@ -933,6 +933,70 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'windwp/nvim-autopairs',
+    event = { 'InsertEnter' },
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+    },
+    config = function()
+      -- import nvim-autopairs
+      local autopairs = require 'nvim-autopairs'
+
+      -- configure autopairs
+      autopairs.setup {
+        check_ts = true, -- enable treesitter
+        ts_config = {
+          lua = { 'string' }, -- don't add pairs in lua string treesitter nodes
+          javascript = { 'template_string' }, -- don't add pairs in javscript template_string treesitter nodes
+          java = false, -- don't check treesitter on java
+        },
+      }
+
+      -- import nvim-autopairs completion functionality
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+
+      -- import nvim-cmp plugin (completions plugin)
+      local cmp = require 'cmp'
+
+      -- make autopairs and completion work together
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+
+  {
+    'numToStr/Comment.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    config = function()
+      -- import comment plugin safely
+      local comment = require 'Comment'
+
+      local ts_context_commentstring = require 'ts_context_commentstring.integrations.comment_nvim'
+
+      -- enable comment
+      comment.setup {
+        -- for commenting tsx, jsx, svelte, html files
+        pre_hook = ts_context_commentstring.create_pre_hook(),
+      }
+    end,
+  },
+
+  {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim' },
+    keys = {
+      { '<leader>xx', '<cmd>TroubleToggle<CR>', desc = 'Open/close trouble list' },
+      { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<CR>', desc = 'Open trouble workspace diagnostics' },
+      { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>', desc = 'Open trouble document diagnostics' },
+      { '<leader>xq', '<cmd>TroubleToggle quickfix<CR>', desc = 'Open trouble quickfix list' },
+      { '<leader>xl', '<cmd>TroubleToggle loclist<CR>', desc = 'Open trouble location list' },
+      { '<leader>xt', '<cmd>TodoTrouble<CR>', desc = 'Open todos in trouble' },
+    },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
